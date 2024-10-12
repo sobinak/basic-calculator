9cbed8e5066918b09e489bd069bbe1fef886c55c
Sobina Kim <sobinak@umich.edu>
2024-10-12 16:58:44 -0400

    Display updates with user input
    -Problems: need to check user input for operators and only allow clicks on buttons to show on display.

--- calculatorApp.js
+++ calculatorApp.js
@@ -1,19 +1,29 @@
 const calculator = document.querySelector(".calculator");
 const calculatorKeys = calculator.querySelector(".calculator-keys");
 
+// TODO
+function calculate() {
+    return "Result";
+}
+
 calculatorKeys.addEventListener("click", (e) => {
-    //! FIX: area around buttons can be pressed and will produce console output
     let key = e.target;
+    let calculatorDisplay = calculator.querySelector(".calculator-display");
+
     if (key.id === "multiply" || key.id === "divide" || key.id === "add" || key.id === "subtract") {
-        console.log("Operator key " + key.textContent + " pressed!")
+        console.log("Operator key " + key.textContent + " pressed!");
+        calculatorDisplay.textContent += key.textContent; //! FIX: make sure user can input operators. "+-+" is valid input right now
     }
     else if (key.id === "equal") {
-        console.log("= was pressed!")
+        console.log("= was pressed!");
+        calculatorDisplay.textContent = calculate();
     }
     else if (key.id === "clear") {
-        console.log("C was pressed!")
+        console.log("C was pressed!");
+        calculatorDisplay.textContent = "";
     }
-    else {
-        console.log("Number " + key.textContent + " pressed!")
+    else { //! FIX: area around buttons can be pressed and will produce console output
+        console.log("Number " + key.textContent + " pressed!");
+        calculatorDisplay.textContent += key.textContent;
     }
 });
